26c4af1dc6f5fcef7f890a03f9264171d8b80b13
Anthony Dodd <Dodd.AnthonyJosiah@gmail.com>
2016-05-19 11:22:53 -0500

    WIP

--- core/drivers.go
+++ core/drivers.go
@@ -10,7 +10,7 @@ type Driver interface {
 	Deps() []*Dep
 
 	// Up
-	// Up sping up a docker machine according to the Bargefile using this driver. Implementations
+	// Up spins up a docker machine according to the Bargefile using this driver. Implementations
 	// of this interface MUST ENSURE that the machine is up and in a usable state whenever this
 	// command is invoked.
 	Up(*Bargefile, cli.Ui) int
@@ -19,9 +19,17 @@ type Driver interface {
 	// Destroy the docker machine running under this driver for the given Bargefile.
 	Destroy(*Bargefile, cli.Ui) int
 
-	// Rebuild
-	// Rebuild the docker machine running under this driver for the given Bargefile.
-	Rebuild(*Bargefile, cli.Ui) int
+	// SSH
+	// SSH into the docker machine defined in this project's Bargefile.
+	SSH(*Bargefile, cli.Ui) int
+
+	// Halt
+	// Halt the docker machine defined in this project's Bargefile.
+	Halt(*Bargefile, cli.Ui) int
+
+	// IP
+	// Get the IP address of the docker machine defined in this project's Bargefile.
+	IP(*Bargefile, cli.Ui) int
 }
 
 // Dep a type describing a needed command-line tool.
